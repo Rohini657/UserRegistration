72bab2ea97f5c53df05017831012e15c8e508a25
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-14 12:18:45 +0530

    LastName

--- UserRegistration1.sh
+++ UserRegistration1.sh
@@ -11,3 +11,16 @@ echo "FirstName Is Valid";
 else
 echo "FirstName Is Not Valid";
 fi
+
+##Use Case2
+##Last Name validation
+echo "Enter The LastName:"
+read LNAME;
+PAT1="^[A-Z]{1}[a-z]{2,}$";
+if [[ $LNAME =~ $PAT1 ]]
+then
+echo "LastName Is Valid";
+else
+echo "LastName Is Not Valid";
+fi
+
