b12d80c90e6902481b4abe837ed1e63ce5ffa8d4
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-14 12:15:14 +0530

    FirstCommit

---
+++ UserRegistration1.sh
@@ -0,0 +1,13 @@
+#User Registration using Pattern Matching
+#!/bin/bash -x
+##Use Case1
+#validate first name
+echo "Enter The FirstName:"
+read FNAME;
+PAT="^[A-Z]{1}[a-z]{2,}$";
+if [[ $FNAME =~ $PAT ]]
+then
+echo "FirstName Is Valid";
+else
+echo "FirstName Is Not Valid";
+fi
